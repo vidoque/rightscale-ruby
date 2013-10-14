dc413ef074db4f0d372b4a64c1a3cdc38e565902
jimfdavies <jimfdavies@googlemail.com>
2013-10-14 14:41:52 +0100

    initial commit

---
+++ test1.rb
@@ -0,0 +1,6 @@
+require 'pp' 
+require 'yaml'
+require 'right_api_client'
+
+@client = RightApi::Client.new(YAML.load_file(File.expand_path('../login.yml', __FILE__)))
+puts "Available methods: #{pp @client.api_methods}"
